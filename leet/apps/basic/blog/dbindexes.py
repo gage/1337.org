001b54cfb77b39efde804980611e2af3d4e940b3
Gage <gage.tseng@geniecapital.com>
2011-12-18 14:34:16 +0800

    add dbindexes

--- leet/apps/basic/blog/dbindexes.py
+++ leet/apps/basic/blog/dbindexes.py
@@ -1,13 +1,13 @@
-#from apps.basic.blog import Post
-#from dbindexer.api import register_index
-#register_index(blog.Post, {'publish': 'year'}) 
-#register_index(blog.Post, {'publish': 'month'}) 
-#register_index(blog.Post, {'publish': 'day'})
-#
-#register_index(blog.Post, {'created': 'year'}) 
-#register_index(blog.Post, {'created': 'month'}) 
-#register_index(blog.Post, {'created': 'day'})
-#
-#register_index(blog.Post, {'modified': 'year'}) 
-#register_index(blog.Post, {'modified': 'month'}) 
-#register_index(blog.Post, {'modified': 'day'}) 
+from basic.blog.models import Post
+from dbindexer.api import register_index
+register_index(Post, {'publish': 'year'}) 
+register_index(Post, {'publish': 'month'}) 
+register_index(Post, {'publish': 'day'})
+
+register_index(Post, {'created': 'year'}) 
+register_index(Post, {'created': 'month'}) 
+register_index(Post, {'created': 'day'})
+
+register_index(Post, {'modified': 'year'}) 
+register_index(Post, {'modified': 'month'}) 
+register_index(Post, {'modified': 'day'}) 
